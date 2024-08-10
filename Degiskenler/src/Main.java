e12c583657a7ff725291f40f96427064223bec15
AkkusEmre34 <akkusemre0321@gmail.com>
2024-08-10 15:39:05 +0300

    Add files via upload

---
+++ Degiskenler/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayısı=12;
+        String mesaj="Öğrenci sayısı : ";
+        System.out.println(mesaj+ogrenciSayısı);
+        System.out.println(mesaj+ogrenciSayısı);
+        System.out.println("öğrenci sayım : "+ ogrenciSayısı);
+        System.out.println("öğrenci sayım : "+ ogrenciSayısı);
+    }
+}
