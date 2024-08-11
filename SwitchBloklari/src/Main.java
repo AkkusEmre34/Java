f1cc10db545ebce4db4d8447c6d7dcf1370599a7
AkkusEmre34 <akkusemre0321@gmail.com>
2024-08-11 16:12:17 +0300

    Add files via upload

---
+++ SwitchBloklari/src/Main.java
@@ -0,0 +1,29 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'A';
+        switch (grade) {
+            case 'A':
+                System.out.println("mukemmel :  gectiniz");
+                break;
+            case 'B':
+                System.out.println("cok iyi :  gectiniz");
+                break;
+            case 'C':
+                System.out.println("iyi  :   gectiniz ");
+                break;
+            case 'D':
+                System.out.println("fena degil:  gectiniz");
+                break;
+            case 'F':
+                System.out.println("maalesef : kaldınız ");
+                break;
+            default:
+                System.out.println("gecersiz not giridiniz");
+
+
+        }
+
+    }
+}
