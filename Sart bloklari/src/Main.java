6002b3252c9520e3c8d98ad20123e5ec1f59682b
AkkusEmre34 <akkusemre0321@gmail.com>
2024-08-11 15:39:45 +0300

    Add files via upload

---
+++ Sart bloklari/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 17;
+        if (sayi < 18) {
+            System.out.println("sayı 18 den kucuktur");
+        } else if (sayi == 18) {
+            System.out.println("sayı 18 e esittir");
+        }
+        if (sayi > 18) {
+            System.out.println("sayı 18 den buyuktur");
+        }
+
+    }
+
+                        }
+
