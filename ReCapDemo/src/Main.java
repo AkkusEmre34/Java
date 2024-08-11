e9f3c6aceb1053f277ca89501f7f506b30eebb16
AkkusEmre34 <akkusemre0321@gmail.com>
2024-08-11 15:55:15 +0300

    Add files via upload

---
+++ ReCapDemo/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       int sayi1=30;
+       int sayi2=40;
+       int sayi3=50;
+       int enBuyuk=sayi3;
+       if (enBuyuk<sayi2)
+           enBuyuk=sayi2;
+       if (enBuyuk<sayi1)
+           enBuyuk=sayi1;
+        System.out.println("en buyuk =" +enBuyuk);
+    }
+}
