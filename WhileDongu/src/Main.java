915f5b570c6071c9876435f2be287368583ce8a2
AkkusEmre34 <akkusemre0321@gmail.com>
2024-08-14 19:19:09 +0300

    Add files via upload

---
+++ WhileDongu/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+int i=1;
+while (i<10) {
+    i++;
+    System.out.println(i);
+
+}
+
+
+    }
+}
