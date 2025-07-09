2467b0ea01d1010a30261193d18dc021f871e215
AkkusEmre34 <akkusemre0321@gmail.com>
2025-07-09 17:00:09 +0300

    Add files via upload

---
+++ donguTekrar/src/Main.java
@@ -0,0 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        char NOT = 'L';
+
+        switch (NOT) {
+            case 'A':
+                System.out.println("mükemmel : gectiniz ");
+                break;
+            case 'B':
+                System.out.println("cok iyi : gectiniz ");
+                break;
+            case 'C':
+                System.out.println("iyi : gectiniz");
+                break;
+            case 'D':
+                System.out.println("maalesef : kaldınız ");
+            break;
+            default:
+                System.out.println("gecersiz harf");
+
+        }
+
+
+
+
+    }
+}
