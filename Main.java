a7e67740f719363f0a152e0b724bf5bb0272c18d
AkkusEmre34 <akkusemre0321@gmail.com>
2025-07-09 16:27:24 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int sayı =78;
+
+        if (sayı>21){
+            System.out.println("sayı 21 den buyuk");
+
+        }
+        if (sayı==21){
+            System.out.println("sayı 21 e eşit");
+        }
+        if (sayı<21){
+            System.out.println("sayı 21 den kucuktur");
+        }
+
+    }
+}
