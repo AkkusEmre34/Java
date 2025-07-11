11eb419342b31391e1de5daa8afec0a601e3bf6d
AkkusEmre34 <akkusemre0321@gmail.com>
2025-07-11 14:43:07 +0300

    Add files via upload

---
+++ StringlerleCalisma/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+         String mesaj= "bugün hava 41 dereceymis.";
+        System.out.println(mesaj);
+        System.out.println(mesaj.indexOf("e"));
+       //index of ta ilk karekter 0 ile baslar
+        System.out.println(mesaj.lastIndexOf("e"));
+        System.out.println(mesaj.concat("cok sıcak"));
+        System.out.println(mesaj.startsWith("b"));
+        System.out.println(mesaj.endsWith("t"));
+        }
+
+}
