54493770c9cf63d1e9a4ef0e10da4eefda684ae0
AkkusEmre34 <akkusemre0321@gmail.com>
2025-12-28 21:01:40 +0300

    Add files via upload

---
+++ METHODCALISMA/src/METHODCALISMA.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class METHODCALISMA {
+    public static void main(String[] args) {
+
+        selamlama();
+        selamlama2("SELİN" );
+        System.out.println(carpsayilari(6,4));
+        System.out.println(isimyazdirma("SELİNNN"));
+    }
+    public static void selamlama(){
+        System.out.println("merhaba java");
+    }
+    public static void selamlama2(String isim){
+        System.out.println("merhaba , " + isim);
+    }
+    public static int carpsayilari (int a , int b){
+     return a*b;
+}
+  public static  String isimyazdirma(String isim){
+        return isim;
+  }
+}
